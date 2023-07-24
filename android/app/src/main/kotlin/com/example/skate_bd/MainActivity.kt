f620d8ef7d4a85e72372f13bc6c0b683d67aafda
tanvir <tanvir.rajib@bjitgroup.com>
2023-07-24 12:50:35 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/skate_bd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skate_bd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/splash.gif'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              color: Colors.black.withOpacity(.2),
+              child: const Column(children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                Text(
+                  "SkateBd",
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: 20, right: 20, top: 0, bottom: 0),
+                      child: Text(
+                        "First skating community in Bangladesh",
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(children: [
+                      Text(
+                        "Developed by ",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      Text(
+                        "Company-X",
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      )
+                    ]),
+                    Text(
+                      "1.0.0",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ]),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
