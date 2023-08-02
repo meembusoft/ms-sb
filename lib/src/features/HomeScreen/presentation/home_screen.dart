562842cbadb83e2be97d7123b8fe284427795092
tanvir <tanvir.rajib@bjitgroup.com>
2023-08-02 13:07:44 +0600

    Add text animation

---
+++ lib/src/features/HomeScreen/presentation/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('This is Home Screen'),
+      ),
+    );
+  }
+}
--- lib/src/features/SplashScreen/presentation/splash_screen.dart
+++ lib/src/features/SplashScreen/presentation/splash_screen.dart
@@ -1,14 +1,75 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:skate_bd/src/features/HomeScreen/presentation/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() {
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<int> _textTypeAnimation;
+
+  final String _displayText = "First skating community in Bangladesh";
+  String _currentText = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the AnimationController
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    // Create a scale animation
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 2.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Create an animation for character indices
+    _textTypeAnimation = IntTween(begin: 0, end: _displayText.length).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Start the animation
+    _controller.forward();
+
+    // Listen for animation value changes
+    _textTypeAnimation.addListener(() {
+      setState(() {
+        _currentText = _displayText.substring(0, _textTypeAnimation.value);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(context) {
+    // Timer(
+    //     const Duration(seconds: 5),
+    //         () =>
+    //         Navigator.of(context).pushReplacement(MaterialPageRoute(
+    //             builder: (BuildContext context) => const HomeScreen())));
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
-        value:
-            SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+        value: SystemUiOverlayStyle.dark
+            .copyWith(statusBarColor: Colors.transparent),
         child: Stack(
           children: [
             Container(
@@ -25,52 +86,66 @@ class SplashScreen extends StatelessWidget {
               height: double.infinity,
               width: double.infinity,
               color: Colors.black.withOpacity(.2),
-              child: const Column(children: <Widget>[
-                SizedBox(
-                  height: 100,
+              child: Column(children: <Widget>[
+                const SizedBox(
+                  height: 160,
                 ),
-                Text(
-                  "SkateBd",
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontWeight: FontWeight.bold,
+                ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: const Text(
+                    'SkateBd',
+                    style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
                 ),
                 Expanded(
                   child: Align(
                     alignment: Alignment.center,
                     child: Padding(
-                      padding: EdgeInsets.only(
+                      padding: const EdgeInsets.only(
                           left: 20, right: 20, top: 0, bottom: 0),
                       child: Text(
-                        "First skating community in Bangladesh",
-                        style: TextStyle(fontSize: 18),
+                        _currentText,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
                         textAlign: TextAlign.center,
                       ),
                     ),
                   ),
                 ),
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
                     Row(children: [
                       Text(
                         "Developed by ",
-                        style: TextStyle(fontSize: 12),
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                        ),
                       ),
                       Text(
                         "Company-X",
                         style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.bold),
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       )
                     ]),
                     Text(
                       "1.0.0",
-                      style: TextStyle(fontSize: 12),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                      ),
                     ),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
               ]),
